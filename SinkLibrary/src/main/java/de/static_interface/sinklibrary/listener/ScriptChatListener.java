9009362157e814dc07c42c8e5e07f658a8dad995
Trojaner <Trojaner25@gmail.com>
2014-03-22 18:22:57 +0200

    Script: Improvements to formatting

--- SinkLibrary/src/main/java/de/static_interface/sinklibrary/listener/ScriptChatListener.java
+++ SinkLibrary/src/main/java/de/static_interface/sinklibrary/listener/ScriptChatListener.java
@@ -154,76 +154,86 @@ public class ScriptChatListener implements Listener
 
     private String formatCode(String code)
     {
-        ChatColor defaultColor = ChatColor.BLUE;
+        ChatColor defaultColor = ChatColor.DARK_BLUE;
         ChatColor codeColor = ChatColor.RESET;
-        ChatColor classColor = ChatColor.AQUA;
+        ChatColor classColor = ChatColor.BLUE;
         ChatColor stringColor = ChatColor.RED;
 
-        code = code.replace("import", ChatColor.GOLD + "import" + codeColor);
-        code = code.replace("package", ChatColor.GOLD + "package" + codeColor);
+        String tmpCode = "";
 
-        code = code.replace("class", defaultColor + "class" + codeColor);
-        code = code.replace("implements", defaultColor + "implements" + codeColor);
-        code = code.replace("extends", defaultColor + "extends" + codeColor);
-        code = code.replace("enum", defaultColor + "enum" + codeColor);
-        code = code.replace("interface", defaultColor + "interface" + codeColor);
+        for ( String word : code.split(" ") )
+        {
+            word = word.replace("import", ChatColor.GOLD + "import" + codeColor);
+            word = word.replace("package", ChatColor.GOLD + "package" + codeColor);
 
-        code = code.replace("private", defaultColor + "private" + codeColor);
-        code = code.replace("public", defaultColor + "public" + codeColor);
-        code = code.replace("protected", defaultColor + "protected" + codeColor);
+            word = word.replace("class", defaultColor + "class" + codeColor);
+            word = word.replace("implements", defaultColor + "implements" + codeColor);
+            word = word.replace("extends", defaultColor + "extends" + codeColor);
+            word = word.replace("enum", defaultColor + "enum" + codeColor);
+            word = word.replace("interface", defaultColor + "interface" + codeColor);
 
-        code = code.replace("final", defaultColor + "final" + codeColor);
-        code = code.replace("static", defaultColor + "static" + codeColor);
-        code = code.replace("native", defaultColor + "native" + codeColor);
-        code = code.replace("throws", defaultColor + "throws" + codeColor);
-        code = code.replace("transient", defaultColor + "transient" + codeColor);
-        code = code.replace("volatile", defaultColor + "volatile" + codeColor);
-        code = code.replace("synchronized", defaultColor + "synchronized" + codeColor);
-        code = code.replace("strictfp", defaultColor + "strictfp" + codeColor);
-        code = code.replace("const", defaultColor + "const" + codeColor);
+            word = word.replace("private", defaultColor + "private" + codeColor);
+            word = word.replace("public", defaultColor + "public" + codeColor);
+            word = word.replace("protected", defaultColor + "protected" + codeColor);
 
-        code = code.replace("try", defaultColor + "try" + codeColor);
-        code = code.replace("catch", defaultColor + "catch" + codeColor);
-        code = code.replace("finally", defaultColor + "finally" + codeColor);
-        code = code.replace("throw", defaultColor + "throw" + codeColor);
+            word = word.replace("final", defaultColor + "final" + codeColor);
+            word = word.replace("static", defaultColor + "static" + codeColor);
+            word = word.replace("native", defaultColor + "native" + codeColor);
+            word = word.replace("throws", defaultColor + "throws" + codeColor);
+            word = word.replace("transient", defaultColor + "transient" + codeColor);
+            word = word.replace("volatile", defaultColor + "volatile" + codeColor);
+            word = word.replace("synchronized", defaultColor + "synchronized" + codeColor);
+            word = word.replace("strictfp", defaultColor + "strictfp" + codeColor);
+            word = word.replace("const", defaultColor + "const" + codeColor);
 
-        code = code.replace("while", defaultColor + "while" + codeColor);
-        code = code.replace("continue", defaultColor + "continue" + codeColor);
+            word = word.replace("try", defaultColor + "try" + codeColor);
+            word = word.replace("catch", defaultColor + "catch" + codeColor);
+            word = word.replace("finally", defaultColor + "finally" + codeColor);
+            word = word.replace("throw", defaultColor + "throw" + codeColor);
 
-        code = code.replace("void", defaultColor + "void" + codeColor);
-        code = code.replace("return", defaultColor + "return" + codeColor);
-        code = code.replace("switch", defaultColor + "switch" + codeColor);
-        code = code.replace("case", defaultColor + "case" + codeColor);
-        code = code.replace("break", defaultColor + "break" + codeColor);
-        code = code.replace("super", defaultColor + "super" + codeColor);
-        code = code.replace("new", defaultColor + "new" + codeColor);
-        code = code.replace("this", defaultColor + "this" + codeColor);
-        code = code.replace("goto", defaultColor + "goto" + codeColor);
+            word = word.replace("while", defaultColor + "while" + codeColor);
+            word = word.replace("continue", defaultColor + "continue" + codeColor);
 
-        code = code.replace("if", defaultColor + "if" + codeColor);
-        code = code.replace("else", defaultColor + "else" + codeColor);
-        code = code.replace("true", defaultColor + "void" + codeColor);
-        code = code.replace("false", defaultColor + "void" + codeColor);
-        code = code.replace("instanceof", defaultColor + "instanceof" + codeColor);
-        code = code.replace("for", defaultColor + "for" + codeColor);
-        code = code.replace("while", defaultColor + "while" + codeColor);
-        code = code.replace("assert", defaultColor + "assert" + codeColor);
+            word = word.replace("void", defaultColor + "void" + codeColor);
+            word = word.replace("return", defaultColor + "return" + codeColor);
+            word = word.replace("switch", defaultColor + "switch" + codeColor);
+            word = word.replace("case", defaultColor + "case" + codeColor);
+            word = word.replace("break", defaultColor + "break" + codeColor);
+            word = word.replace("super", defaultColor + "super" + codeColor);
+            word = word.replace("new", defaultColor + "new" + codeColor);
+            word = word.replace("this", defaultColor + "this" + codeColor);
+            word = word.replace("goto", defaultColor + "goto" + codeColor);
 
-        code = code.replace("String", classColor + "String" + codeColor);
-        code = code.replace("Bukkit", classColor + "Bukkit" + codeColor);
-        code = code.replace("BukkitUtil", classColor + "BukkitUtil" + codeColor);
-        code = code.replace("SinkLibrary", classColor + "SinkLibrary" + codeColor);
-        code = code.replace("User", classColor + "User" + codeColor);
-        code = code.replace("Logger", classColor + "Logger" + codeColor);
-        code = code.replace("Bukkit", classColor + "Bukkit" + codeColor);
+            word = word.replace("if", defaultColor + "if" + codeColor);
+            word = word.replace("else", defaultColor + "else" + codeColor);
+            word = word.replace("true", defaultColor + "void" + codeColor);
+            word = word.replace("false", defaultColor + "void" + codeColor);
+            word = word.replace("instanceof", defaultColor + "instanceof" + codeColor);
+            word = word.replace("for", defaultColor + "for" + codeColor);
+            word = word.replace("while", defaultColor + "while" + codeColor);
+            word = word.replace("assert", defaultColor + "assert" + codeColor);
 
-        code = code.replace("int", defaultColor + "int" + codeColor);
-        code = code.replace("boolean", defaultColor + "boolean" + codeColor);
-        code = code.replace("long", defaultColor + "long" + codeColor);
-        code = code.replace("short", defaultColor + "short" + codeColor);
-        code = code.replace("float", defaultColor + "float" + codeColor);
-        code = code.replace("byte", defaultColor + "byte" + codeColor);
-        code = code.replace("char", defaultColor + "char" + codeColor);
+            word = word.replace("String", classColor + "String" + codeColor);
+            word = word.replace("Bukkit", classColor + "Bukkit" + codeColor);
+            word = word.replace("BukkitUtil", classColor + "BukkitUtil" + codeColor);
+            word = word.replace("SinkLibrary", classColor + "SinkLibrary" + codeColor);
+            word = word.replace("User", classColor + "User" + codeColor);
+            word = word.replace("Logger", classColor + "Logger" + codeColor);
+            word = word.replace("Player", classColor + "Player" + codeColor);
+            word = word.replace("Plugin", classColor + "Plugin" + codeColor);
+
+            word = word.replace("int", defaultColor + "int" + codeColor);
+            word = word.replace("boolean", defaultColor + "boolean" + codeColor);
+            word = word.replace("long", defaultColor + "long" + codeColor);
+            word = word.replace("short", defaultColor + "short" + codeColor);
+            word = word.replace("float", defaultColor + "float" + codeColor);
+            word = word.replace("byte", defaultColor + "byte" + codeColor);
+            word = word.replace("char", defaultColor + "char" + codeColor);
+
+            tmpCode += word + ' ';
+        }
+
+        code = tmpCode;
 
         boolean stringStart = false;
 
@@ -231,7 +241,6 @@ public class ScriptChatListener implements Listener
         String tmp = "";
         for ( char Char : code.toCharArray() )
         {
-            tmp += Char;
             if ( Char == '"' )
             {
                 if ( !stringStart )
@@ -239,7 +248,10 @@ public class ScriptChatListener implements Listener
                     tmp += stringColor;
                     stringStart = true;
                 }
-                else
+
+                tmp += Char;
+
+                if ( stringStart )
                 {
                     stringStart = false;
                     tmp += codeColor;
