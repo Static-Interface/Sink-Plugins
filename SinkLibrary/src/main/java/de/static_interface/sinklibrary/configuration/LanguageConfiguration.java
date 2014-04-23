7e801c8a984c1fe5836ff2d77ead87941f72d817
Trojaner <Trojaner25@gmail.com>
2014-04-23 14:04:25 +0300

    Fixed that PlayerConfiguration didn't load

--- SinkLibrary/src/main/java/de/static_interface/sinklibrary/configuration/LanguageConfiguration.java
+++ SinkLibrary/src/main/java/de/static_interface/sinklibrary/configuration/LanguageConfiguration.java
@@ -38,6 +38,7 @@ public class LanguageConfiguration extends ConfigurationBase
         if ( instance == null )
         {
             instance = new LanguageConfiguration();
+            instance.load();
         }
         return instance;
     }
