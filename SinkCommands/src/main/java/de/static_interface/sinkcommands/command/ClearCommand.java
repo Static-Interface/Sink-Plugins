998330c7b46d876383b01be7c137a36c0b6efc5e
Trojaner <trojaner25@gmail.com>
2014-12-26 17:33:01 +0200

    Rewrite clear command with CLI

--- SinkCommands/src/main/java/de/static_interface/sinkcommands/command/ClearCommand.java
+++ SinkCommands/src/main/java/de/static_interface/sinkcommands/command/ClearCommand.java
@@ -17,18 +17,17 @@
 
 package de.static_interface.sinkcommands.command;
 
-import de.static_interface.sinklibrary.SinkLibrary;
-import de.static_interface.sinklibrary.user.IngameUser;
-import de.static_interface.sinklibrary.api.command.SinkCommand;
-import de.static_interface.sinklibrary.util.BukkitUtil;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.potion.PotionEffectType;
+import static de.static_interface.sinklibrary.configuration.LanguageConfiguration.*;
 
-import java.util.Arrays;
-import java.util.List;
+import de.static_interface.sinklibrary.*;
+import de.static_interface.sinklibrary.api.command.*;
+import de.static_interface.sinklibrary.user.*;
+import org.apache.commons.cli.*;
+import org.bukkit.*;
+import org.bukkit.command.*;
+import org.bukkit.entity.*;
+import org.bukkit.plugin.*;
+import org.bukkit.potion.*;
 
 public class ClearCommand extends SinkCommand {
     //Todo: convert to command
@@ -38,58 +37,95 @@ public class ClearCommand extends SinkCommand {
     public ClearCommand(Plugin plugin) {
         super(plugin);
         getCommandOptions().setPlayerOnly(true);
+        getCommandOptions().setCliOptions(buildOptions());
+        getCommandOptions().setCmdLineSyntax("{PREFIX}{ALIAS} <options>");
+    }
+
+    private Options buildOptions() {
+        Options options = new Options();
+        Option inventory = Option.builder("i")
+                .desc("Remove inventory")
+                .longOpt("inventory")
+                .build();
+
+        Option effects = Option.builder("e")
+                .longOpt("effects")
+                .desc("Clear effects")
+                .build();
+
+        Option armor = Option.builder("a")
+                .longOpt("armor")
+                .desc("Clear armor")
+                .build();
+
+        Option xp = Option.builder("x")
+                .longOpt("xp")
+                .desc("Clear XP")
+                .build();
+
+        Option all = Option.builder("A")
+                .longOpt("all")
+                .desc("Clear everything")
+                .build();
+
+        Option target = Option.builder("t")
+                .longOpt("target")
+                .desc("Target player")
+                .hasArgs()
+                .argName("player")
+                .type(String.class)
+                .build();
+
+        options.addOption(inventory);
+        options.addOption(effects);
+        options.addOption(armor);
+        options.addOption(xp);
+        options.addOption(all);
+        options.addOption(target);
+
+        return options;
     }
 
     @Override
-    public boolean onExecute(CommandSender sender, String label, String[] argsArr) {
-        List<String> args = Arrays.asList(argsArr);
+    public boolean onExecute(CommandSender sender, String label, String[] argsArr) throws ParseException {
+        Player player = (Player) sender;
 
-        Player player;
         IngameUser user = (IngameUser) SinkLibrary.getInstance().getUser(sender);
 
-        boolean clearInvetory = false;
-        boolean clearEffects = false;
-        boolean clearArmor = false;
-        int i = 0;
-        if (args.contains("-i") || args.contains("-inventory")) {
-            clearInvetory = true;
-            i++;
-        }
-        if (args.contains("-e") || args.contains("-effects")) {
-            clearEffects = true;
-            i++;
-        }
-        if (args.contains("-ar") || args.contains("-armor")) {
-            clearArmor = true;
-            i++;
-        }
-        if (args.contains("-a") || args.contains("-all")) {
-            clearInvetory = true;
-            clearEffects = true;
-            clearArmor = true;
-            i = 3;
-        }
-        if (!clearInvetory && !clearEffects && !clearArmor) {
-            clearInvetory = true;
-            i = 1;
-        }
-
-        if (argsArr.length >= 1) {
-            String name = argsArr[i];
+        if (getCommandLine().hasOption('t')) {
             if (!user.hasPermission("sinkcommands.clear.others")) {
-                sender.sendMessage(PREFIX + "Du hast nicht genügend Rechte um das Inventar von anderen Spielern zu leeren!");
+                sender.sendMessage(PREFIX + m("Permissions.General"));
                 return true;
             }
-            player = BukkitUtil.getPlayer(name);
+
+            String targetName = getCommandLine().getParsedOptionValue("t").toString();
+
+            player = Bukkit.getPlayer(targetName);
+
             if (player == null) {
-                sender.sendMessage(PREFIX + "Spieler wurde nicht gefunden: " + name);
+                sender.sendMessage(PREFIX + m("General.NotOnline", targetName));
                 return true;
             }
         }
 
-        player = user.getPlayer();
+        boolean clearInventory;
+        boolean clearEffects;
+        boolean clearArmor;
+        boolean clearXp;
 
-        if (clearInvetory) {
+        if (getCommandLine().hasOption('A')) {
+            clearInventory = true;
+            clearEffects = true;
+            clearArmor = true;
+            clearXp = true;
+        } else {
+            clearInventory = getCommandLine().hasOption('i');
+            clearEffects = getCommandLine().hasOption('e');
+            clearArmor = getCommandLine().hasOption('a');
+            clearXp = getCommandLine().hasOption('x');
+        }
+
+        if (clearInventory) {
             player.getInventory().clear();
         }
 
@@ -118,12 +154,18 @@ public class ClearCommand extends SinkCommand {
             player.removePotionEffect(PotionEffectType.WITHER);
             player.removePotionEffect(PotionEffectType.FIRE_RESISTANCE);
         }
+
         if (clearArmor) {
             player.getInventory().setHelmet(null);
             player.getInventory().setChestplate(null);
             player.getInventory().setLeggings(null);
             player.getInventory().setBoots(null);
         }
+
+        if (clearXp) {
+            player.setTotalExperience(0);
+        }
+
         if (!player.equals(sender)) {
             sender.sendMessage(PREFIX + player.getDisplayName() + " wurde gecleart.");
         }
