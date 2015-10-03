f4644067a4364102d35c72de56dd3a4f5e68ec97
Trojaner_ <trojaner25@gmail.com>
2015-10-03 23:55:06 +0300

    Implement SinkUserMessageStream

---
+++ SinkLibrary/src/main/java/de/static_interface/sinklibrary/api/stream/SinkUserMessageStream.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2013 - 2015 http://static-interface.de and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.static_interface.sinklibrary.api.stream;
+
+import de.static_interface.sinklibrary.api.user.SinkUser;
+import de.static_interface.sinklibrary.user.IngameUser;
+import de.static_interface.sinklibrary.util.StringUtil;
+
+import javax.annotation.Nullable;
+
+public class SinkUserMessageStream extends MessageStream<IngameUser> {
+
+    private final SinkUser user;
+
+    public SinkUserMessageStream(@Nullable SinkUser user) {
+        super("sinkuser_" + user.getName().toLowerCase());
+        this.user = user;
+    }
+
+    @Override
+    protected boolean onSendMessage(@Nullable IngameUser sender, String message) {
+        message = StringUtil.format(message, sender, null);
+
+        if (!user.isOnline()) {
+            return false;
+        }
+        user.sendMessage(message);
+        return true;
+    }
+}
--- SinkLibrary/src/main/java/de/static_interface/sinklibrary/api/user/SinkUser.java
+++ SinkLibrary/src/main/java/de/static_interface/sinklibrary/api/user/SinkUser.java
@@ -17,8 +17,9 @@
 
 package de.static_interface.sinklibrary.api.user;
 
+import de.static_interface.sinklibrary.SinkLibrary;
 import de.static_interface.sinklibrary.api.command.SinkCommand;
-import de.static_interface.sinklibrary.api.configuration.Configuration;
+import de.static_interface.sinklibrary.api.stream.SinkUserMessageStream;
 import de.static_interface.sinklibrary.configuration.GeneralSettings;
 import de.static_interface.sinklibrary.util.Debug;
 import org.apache.commons.lang3.Validate;
@@ -30,6 +31,7 @@ public abstract class SinkUser<T> implements Comparable<SinkUser> {
 
     SinkUserProvider provider;
     private T base;
+    private SinkUserMessageStream messageStream;
 
     public SinkUser(T base, SinkUserProvider provider) {
         Validate.notNull(base);
@@ -37,6 +39,12 @@ public abstract class SinkUser<T> implements Comparable<SinkUser> {
 
         this.provider = provider;
         this.base = base;
+        messageStream = new SinkUserMessageStream(this);
+        SinkLibrary.getInstance().registerMessageStream(messageStream);
+    }
+
+    protected SinkUserMessageStream getMessageStream() {
+        return messageStream;
     }
 
     public T getBase() {
